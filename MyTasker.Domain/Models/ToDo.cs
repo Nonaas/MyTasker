02f18a0cbd225584263137d4c986b1b1bfdbac74
Jonas Wahle <wahle.jonas@web.de>
2024-11-21 14:24:56 +0100

    Fixed Bullshit

--- MyTasker.Domain/Models/ToDo.cs
+++ MyTasker.Domain/Models/ToDo.cs
@@ -4,7 +4,8 @@
     {
         public string Description { get; set; }
         public bool Done { get; set; }
-
+        public bool Disabled { get; set; } = false;
+         
         public ToDo(string description, bool done)
         {
             Description = description;
