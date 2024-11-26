ec839e736879d23d1d660cfb25e715d6d9c15a12
Jonas Wahle <wahle.jonas@web.de>
2024-11-26 17:56:28 +0100

    Safe Checkpoint

---
+++ MyTasker/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:MyTasker.App.CreateWindow(Microsoft.Maui.IActivationState)~Microsoft.Maui.Controls.Window")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:MyTasker.MauiProgram.CreateMauiApp~Microsoft.Maui.Hosting.MauiApp")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:MyTasker.MainApplication.#ctor(System.IntPtr,Android.Runtime.JniHandleOwnership)")]
