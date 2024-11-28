38c7162a96cce326c9e9794aa189258c97fd6d2e
Jonas Wahle <wahle.jonas@web.de>
2024-11-28 18:19:39 +0100

    Update SendNotification() params

--- MyTasker.Domain/Services/Interfaces/INotificationManagerService.cs
+++ MyTasker.Domain/Services/Interfaces/INotificationManagerService.cs
@@ -3,7 +3,7 @@
     public interface INotificationManagerService
     {
         event EventHandler NotificationReceived;
-        void SendNotification(string title, string message, DateTime? notifyTime = null);
+        void SendNotification(string title, string message, DateTime? notifyTime = null, Dictionary<string, string>? args = null);
         void ReceiveNotification(string title, string message);
     }
 }
